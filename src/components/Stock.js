9419630eb9524a79e08812e3137554de92f0daa8
Daniel Hallada <hallada.daniel@gmail.com>
2019-06-11 16:31:50 -0400

    Done.

--- src/components/Stock.js
+++ src/components/Stock.js
@@ -1,20 +1,18 @@
 import React from 'react'
 
-const Stock = () => (
-  <div>
+const Stock = (props) => (
 
-    <div className="card">
+  <div>
+    <div className="card" onClick={() => props.handleClick(props.stockInfo)}>
       <div className="card-body">
         <h5 className="card-title">{
-            //Company Name
+            props.stockInfo.name
           }</h5>
         <p className="card-text">{
-            //ticker: stock price
+            props.stockInfo.price
           }</p>
       </div>
     </div>
-
-
   </div>
 );
 
--- src/containers/MainContainer.js
+++ src/containers/MainContainer.js
@@ -4,21 +4,72 @@ import PortfolioContainer from './PortfolioContainer'
 import SearchBar from '../components/SearchBar'
 
 class MainContainer extends Component {
+  constructor() {
+    super()
+    this.state = {
+      allStocks: [],
+      myPortfolio: [],
+      filterChoice:"",
+      sortChoice: null
+    }
+  }
+
+  handleSort = (event) => {
+    let stocksCopy = this.state.allStocks
+    if (event.target.value === "Alphabetically") {
+      this.setState({
+        allStocks: stocksCopy.sort((a,b) => (a.name > b.name) ? 1 : ((b.name > a.name) ? -1 : 0))
+      })
+    }
+    else if (event.target.value === "Price") {
+      this.setState({
+       allStocks: stocksCopy.sort((a,b) => a.price - b.price)
+      })
+    }
+  }
+
+  handleFilterChoice = (event) => {
+    this.setState({
+      filterChoice: event.target.value
+    })
+  }
+
+  removeFromPortfolio = (stockObj) => {
+    let updatedPortfolio = this.state.myPortfolio.filter(stock => stock !== stockObj)
+    this.setState({
+      myPortfolio: updatedPortfolio
+    })
+  }
+
+  addToPortfolio = (stockObj) => {
+    let currentPortfolio = this.state.myPortfolio 
+    currentPortfolio.push(stockObj)
+
+    this.setState({
+      myPortfolio: currentPortfolio
+    })
+  }
+
+  componentDidMount() {
+    fetch("http://localhost:3000/stocks")
+    .then(res => res.json())
+    .then(stocks => this.setState({allStocks: stocks}))
+  }
 
   render() {
     return (
       <div>
-        <SearchBar/>
+        <SearchBar handleFilterChoice={this.handleFilterChoice} handleSort={this.handleSort}/>
 
           <div className="row">
             <div className="col-8">
 
-              <StockContainer/>
+              <StockContainer stocks={this.state.allStocks.filter(stock => stock.type.includes(this.state.filterChoice))} handleClick={this.addToPortfolio}/>
 
             </div>
             <div className="col-4">
 
-              <PortfolioContainer/>
+              <PortfolioContainer portfolio={this.state.myPortfolio} handleClick={this.removeFromPortfolio}/>
 
             </div>
           </div>
